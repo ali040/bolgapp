52ac100f28fd06f7d5c2d3f1dd2f1c411bba59a7
ali040 <alidumair@gmail.com>
2021-12-01 15:08:59 +0500

    profilepic

--- app/src/main/java/com/example/mynews/HomeActivity.java
+++ app/src/main/java/com/example/mynews/HomeActivity.java
@@ -1,28 +1,44 @@
 package com.example.mynews;
 
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.util.UUID;
 
 public class HomeActivity extends AppCompatActivity {
     TextView fullName, email , phone, verifyMsg,verifybtn ;
     FirebaseAuth fAuth;
     FirebaseFirestore fstore;
     String userID;
-
+    private ImageView profilePic;
+    private Uri imageuri;
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +51,12 @@ public class HomeActivity extends AppCompatActivity {
         fstore= FirebaseFirestore.getInstance();
         verifybtn=(TextView)findViewById(R.id.verifybtn);
         verifyMsg=(TextView)findViewById(R.id.notverifymsg);
+
+
+        profilePic =(ImageView)findViewById(R.id.profilepic);
+
+        storage =FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
         userID = fAuth.getCurrentUser().getUid();
 
@@ -74,38 +96,97 @@ public class HomeActivity extends AppCompatActivity {
 
 
         }
-//        if(!user.isEmailVerified()){
-//            verifyMsg.setVisibility(View.VISIBLE);
-//            resendcode.setVisibility(View.VISIBLE);
-//
-//            resendcode.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View view) {
-//
-//                    user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
-//                        @Override
-//                        public void onSuccess(Void unused) {
-//                            Toast.makeText(view.getContext(), "Verification Link Sent to Email", Toast.LENGTH_SHORT).show();
-//                        }
-//                    }).addOnFailureListener(new OnFailureListener() {
-//                        @Override
-//                        public void onFailure(@NonNull Exception e) {
-//                            Toast.makeText(view.getContext(),"Error ! Verification Link is Not Sent "+e.getMessage(),Toast.LENGTH_SHORT).show();
-//
-//                        }
-//                    });
-//
-//                }
-//            });
-//        }
+        profilePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chosePicture();
 
-//        DocumentReference documentReference = fstore.collection("user").document(userID);
-//        documentReference.addSnapshotListener(HomeActivity.this,(documentSnapshot,e)->{
-//           phone.setText(documentSnapshot.getString("phone"));
-//           fullName.setText(documentSnapshot.getString("fName"));
-//           email.setText(documentSnapshot.getString("email"));
+            }
+        });
+
+//        profilePic.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                chosePicture();
+//            }
 //        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1&& resultCode == RESULT_OK&&data!=null&&data.getData()!=null){
+            imageuri = data.getData();
+            profilePic.setImageURI(imageuri);
+            UploadPicture();
+
+        }
+    }
+
+    private void UploadPicture() {
+        final  ProgressDialog pd =new ProgressDialog(this);
+        final String randomkey = UUID.randomUUID().toString();
+       StorageReference imageRef = storageReference.child("image/"+randomkey);
+
+       imageRef.putFile(imageuri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+           @Override
+           public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+               pd.dismiss();
+
+           }
+       }).addOnFailureListener(new OnFailureListener() {
+           @Override
+           public void onFailure(@NonNull Exception e) {
+               pd.dismiss();
+
+           }
+       }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+           @Override
+           public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+               double progresspercent = (100.00+ snapshot.getBytesTransferred()/snapshot.getTotalByteCount());
+             pd.setMessage("percente " +(int) progresspercent + '%');
+
+           }
+       });
+    }
+
+    private void chosePicture() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,1);
+    }
+
+
+
+
+//    private void uploadPicture() {
+//        final ProgressDialog pd = new ProgressDialog(this);
+//        final String randomKey = UUID.randomUUID().toString();
+//     StorageReference riversRef = storageReference.child("image/"+randomKey);
+//
+//     riversRef.putFile(imageuri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+//         @Override
+//         public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+//             pd.dismiss();
+//             //Toast.makeText(view.getContext(), "Verification Link Sent to Email", Toast.LENGTH_SHORT).show();
+//
+//
+//         }
+//     }).addOnFailureListener(new OnFailureListener() {
+//         @Override
+//         public void onFailure(@NonNull Exception e) {
+//             pd.dismiss();
+//
+//         }
+//     }).addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+//         @Override
+//         public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+//             double progresspercent = (100.00+ snapshot.getBytesTransferred()/snapshot.getTotalByteCount());
+//             pd.setMessage("percente " +(int) progresspercent + '%');
+//         }
+//     });
+//    }
 
     public void logout(View view){
         FirebaseAuth.getInstance().signOut();
